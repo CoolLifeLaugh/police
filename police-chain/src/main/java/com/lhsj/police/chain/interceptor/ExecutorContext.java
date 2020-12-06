7143d3020fe382f436f851daa7d39a6457bd5748
l_h_s_j <l_h_s_j@163.com>
2020-12-06 18:10:49 +0800

    Context

--- police-chain/src/main/java/com/lhsj/police/chain/interceptor/ExecutorContext.java
+++ police-chain/src/main/java/com/lhsj/police/chain/interceptor/ExecutorContext.java
@@ -1,4 +1,16 @@
 package com.lhsj.police.chain.interceptor;
 
+import com.lhsj.police.chain.ChainResult;
+
 public class ExecutorContext {
+
+    private ChainResult result = new ChainResult();
+
+    public ChainResult getResult() {
+        return result;
+    }
+
+    public void setResult(ChainResult result) {
+        this.result = result;
+    }
 }
